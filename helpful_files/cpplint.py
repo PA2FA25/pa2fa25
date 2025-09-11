01647cd0a0a350172dbd6a2393a936f19e077b10
Luke Sathrum <sathrumlu@butte.edu>
2025-09-11 10:34:39 -0700

    Adding labs 8 and 9

--- helpful_files/cpplint.py
+++ helpful_files/cpplint.py
@@ -3,6 +3,7 @@
 # Copyright (c) 2009 Google Inc. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
+#
 # modification, are permitted provided that the following conditions are
 # met:
 #
